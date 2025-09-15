b89ad5b6cb0b0c160c14841ed66396b366676c86
jskang6001 <jskang6001@naver.com>
2025-09-15 10:52:46 +0900

    feat: add rootpageUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
-import NavBar from "../components/NavBar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <body className="min-h-screen flex flex-col bg-gradient-to-r from-rose-quartz-500 to-serenity-500">
-        <NavBar />
         <div className="flex-1 flex items-center justify-center">
           {children}
         </div>
