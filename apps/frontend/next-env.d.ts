e3269d3221fc4d18cc8ba46802238ca85e779dd7
jskang6001 <jskang6001@naver.com>
2025-09-19 18:06:53 +0900

    feat : 버튼 애니메이션 추가, 글자 크기 확대, 날개아이콘 추가

--- apps/frontend/next-env.d.ts
+++ apps/frontend/next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: 이 파일은 Next.js가 자동으로 관리합니다.
-// 직접 수정하지 말고, 만약 삭제됐다면 다시 만들어 주면 됩니다.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,26 +1,39 @@
+"use client";
+import Image from "next/image";
+
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
     <div className="text-center">
-      <h1 className="text-9xl md:text-12xl font-extrabold tracking-tight text-white drop-shadow">
+      <h1 className="relative top-[-10px] text-[12rem] md:text-12xl font-extrabold tracking-tight text-white drop-shadow -mt-[40px]">
         AIL-ways
       </h1>
 
-      <p className="text-2xl mt-3 text-white/90">학습에 날개를 달아보세요🪽</p>
+        <p className="relative -top-5 flex items-baseline justify-center text-2xl mt-3 text-white/90 gap-2">
+          <Image src="/wing_left.png" alt="왼쪽 날개" width={100} height={100} className="relative top-4 left-3"/>
+              학습에 날개를 달아보세요!
+          <Image src="/wing_right.png" alt="오른쪽 날개" width={100} height={100} className="relative top-4 right-3" />
+        </p>
+
 
       <div className="mt-8">
+        <motion.div
+        animate={{ rotate: [0, -5, 5, -5, 0] }} // 좌우로 흔들림
+        transition={{ repeat: Infinity, duration: 2, ease: "easeInOut" }}
+      >
         <Link
-          href="/signin"
+          href="/login"
           className="inline-block rounded-full px-10 py-4 text-lg font-semibold 
-                     bg-white/90 text-gray-900 shadow-lg 
+                     bg-white/50  text-gray-900 shadow-lg 
                      hover:bg-white transition 
-                     focus:outline-none focus:ring-2 focus:ring-white/70 
+                     focus:outline-none focus:ring-2 focus:ring-white/70
                      active:scale-95"
-          aria-label="시작하기"
-        >
+          >
           시작하기
-        </Link>
+          </Link>
+        </motion.div>
       </div>
     </div>
   );
