b64fed546b2261744d75bf6522f8c288d99ee751
jskang6001 <jskang6001@naver.com>
2025-09-19 22:29:27 +0900

    feat : 로그인 화면 구현

---
+++ apps/frontend/src/app/login/page.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-white/70 p-8 rounded-2xl shadow-lg text-center w-full max-w-sm">
+      <div className="grid gap-3">
+        <Link
+          href="/auth/kakao" // /api/auth/signin/kakao, 직접 구현이면 백엔드 시작점으로 변경
+          className="relative flex item-center w-full justify-center rounded-xl px-4 py-3 font-medium bg-[#FEE500] text-black hover:brightness-95 transition inline-block">
+          <Image src="/kakao_logo.png" alt="Kakao" width={23} height={23} className="absolute left-5 top-3.5"/>
+          <span className="leading-none">카카오로 로그인</span>
+        </Link>
+
+        <Link
+          href="/auth/google" // /api/auth/signin/google 로 변경
+          className="relative flex item-center justify-center w-full rounded-xl px-4 py-3 font-medium bg-white text-gray-800 border border-gray-300 hover:bg-gray-50 transition inline-block">
+          <Image src="/google_logo.png" alt="google" width={20} height={20} className="absolute left-5 top-4"/>
+          <span className="leading-none">구글로 로그인</span>
+        </Link>
+
+        <Link
+          href="/login" // ← 이메일 회원가입 경로로 변경해야함.
+          className="relative flex item-center justify-center w-full rounded-xl px-4 py-3 font-medium bg-indigo-500 text-white hover:bg-indigo-500 transition inline-block">
+          <Image src="/letter.png" alt="letter" width={23} height={23} className="absolute left-5 top-3"/>
+          이메일로 로그인
+        </Link>
+
+        <div className="h-[1px] bg-gray-300"/>
+        <div className="relative">
+          <span className="absolute left-1/2 -translate-x-1/2 -top-1 text-xs text-gray-500 rounded">
+            회원이 아니신가요?{" "}
+              <Link href="/signup" className="text-blue-500 hover:underline">
+                회원가입
+              </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
