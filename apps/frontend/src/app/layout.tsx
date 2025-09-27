bcd0a83634d309a9f331ffcf56eac9364388fa0e
jskang6001 <jskang6001@naver.com>
2025-09-27 22:30:55 +0900

    페이지마다 헤더 적용

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,11 +1,21 @@
-import "./globals.css";
+"use client"
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "./globals.css";
+import { Header } from "@/components/layout/Header";
+import { usePathname } from "next/navigation";
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) { //children은 모든 타입 다 수용
+  const pathname = usePathname();
+
+  const hideHeaderRoutes = ["/", "/login", "/signup", "/select"];
+
   return (
     <html lang="ko">
       <body className="min-h-screen flex flex-col bg-gradient-to-r from-rose-quartz-500 to-serenity-500">
+          {!hideHeaderRoutes.includes(pathname) && <Header />} 
         <div className="flex-1 flex items-center justify-center">
-          {children}
+          {children}    {/*부모가 넘긴 페이지 내용이 그대로 꽃힘 */}
         </div>
       </body>
     </html>
