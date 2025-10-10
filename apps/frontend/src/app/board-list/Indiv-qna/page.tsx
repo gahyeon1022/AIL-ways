ead652e3449a5f68830d63a4593fa53072a14181
SEUNGJUN HA <133952466+seunG-Zzun@users.noreply.github.com>
2025-10-10 23:05:04 +0900

    chore: front 최신 반영

--- apps/frontend/src/app/board-list/Indiv-qna/page.tsx
+++ apps/frontend/src/app/board-list/Indiv-qna/page.tsx
@@ -1,5 +1,5 @@
 
-import QnaUI from "@/components/ui/Qna"; // tsconfig에 baseUrl=src, paths가 잡혀있다는 가정
+import QnaUI from "@/app/board-list/Indiv-qna/Qna"; // tsconfig에 baseUrl=src, paths가 잡혀있다는 가정
 // 만약 경로 alias가 없다면: import QnaUI from "../../components/QnaUI";
 
 export default function Page() {
