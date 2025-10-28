9ea406ba2cf7a2a3abb84d82b5407183b31d725d
jskang6001 <jskang6001@naver.com>
2025-10-28 17:21:36 +0900

    재사용을 위해 components폴더로 이동

--- apps/frontend/src/app/signup/components/LocalSignupModal.tsx
+++ apps/frontend/src/app/signup/components/LocalSignupModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ScrollableTerms from "@/app/signup/components/ScrollableTerms";
+import ScrollableTerms from "@/components/ui/ScrollableTerms";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
