c9e1b147c476c9385acf02e2462ade8151b5e90c
jskang6001 <jskang6001@naver.com>
2025-11-16 00:13:45 +0900

    feat : 네비게이션 간격 조정

--- apps/frontend/src/components/layout/header/Header.tsx
+++ apps/frontend/src/components/layout/header/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
 
         {/* Navigation */}
         <nav className="flex h-full items-center justify-center space-x-10 text-[20px] text-white/90 px-8">
-          <Link href="/mentoring-current" className="group inline-flex flex-col items-center">
+          <Link href="/mentoring-current" className="group inline-flex flex-col items-center mr-4">
             <Button variant="ghost" className="text-[20px] font-normal w-[150px] justify-center">
               멘토링 현황
             </Button>
@@ -35,7 +35,7 @@ export default function Header() {
             </Button>
             <span className="mt-1 block h-[2px] w-full max-w-[70px] rounded-full bg-white/50 transition group-hover:bg-white" />
           </Link>
-          <Link href="/mentoring-current?intent=board" className="group inline-flex flex-col items-center -ml-4">
+          <Link href="/mentoring-current?intent=board" className="group inline-flex flex-col items-center -ml-10">
             <Button variant="ghost" className="text-[20px] font-normal w-[150px] justify-center">
               게시판
             </Button>
